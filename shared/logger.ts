2735fcee8f080f69e243a84fc8fbdf847c63b457
Robin <robin@WIL-Admins-MacBook-Pro.local>
2023-11-29 17:52:53 +0530

    fix: logger issue

--- shared/logger.ts
+++ shared/logger.ts
@@ -2,7 +2,7 @@ import pino from 'pino'
 import expressPino from 'express-pino-logger'
 
 export const logger = pino({
-  level: process.env.LOG_LEVEL || 'info',
+  level: 'info',
 })
 
 export const expressLogger = expressPino({
