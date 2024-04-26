ad839f216b6d99c86a3261a0376ea041a852e45a
HtDeepMakadiya <deep.makadiya@hanabitech.com>
2024-04-26 12:26:41 +0530

    refactor: use trudesk controller

--- controller/bugzilla.controller.ts
+++ controller/bugzilla.controller.ts
@@ -1,26 +1,28 @@
 import { Response, Request } from 'express'
 import { logger } from '../shared/logger'
-import BugzillaBugService from '../services/bugzilla.service'
+// import BugzillaBugService from '../services/bugzilla.service'
+import TrudeskTicketService from '../services/trudesk.service'
 
-const BugService = new BugzillaBugService()
+// const BugService = new BugzillaBugService();
+const TrudeskService = new TrudeskTicketService();
 
 class BugsController {
   async createBug(req: Request, res: Response) {
-    const data = await BugService.createBug(req, res)
-     logger.info('POST Bugzilla Endpoint hit with: ' + req.body)
+    const data = await TrudeskService.createBug(req, res)
+     logger.info('POST Trudesk Endpoint hit with: ' + req.body)
     return data
   }
 
   async getAllBug(req: Request, res: Response) {
-    const data = await BugService.getBug(req, res)
+    const data = await TrudeskService.getBug(req, res)
 
-    logger.info('POST Bugzilla Endpoint hit with: ' + req.body)
+    logger.info('POST Trudesk Endpoint hit with: ' + req.body)
     return data
   }
 
   async updateBug(req: Request, res: Response) {
-    const data = await BugService.updateBug(req, res)
-    logger.info('Put Bugzilla Endpoint hit with: ' + JSON.stringify(req.body))
+    const data = await TrudeskService.updateBug(req, res)
+    logger.info('Put Trudesk Endpoint hit with: ' + JSON.stringify(req.body))
     return data
   }
 }
